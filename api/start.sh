306ab00d9f5878a841e2335d4ed5be0e37294ecf
oligregz <gregorysak90@gmail.com>
2024-11-24 14:15:45 -0300

    build: resolve conflicts

--- api/start.sh
+++ api/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 wait-for-it db-container-chega-facil:5432 --timeout=30 -- echo "Database done."
 
-npx prisma migrate dev --name "initial_migration"
-npx prisma generate
-
 # Start application
-npm run start:dev
+npm run docker:start
