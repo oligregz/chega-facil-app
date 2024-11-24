965f3c3c51bd18f447b94b3479671c87d8754825
oligregz <gregorysak90@gmail.com>
2024-11-24 18:26:25 -0300

    feat: add request logs

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
@@ -7,6 +7,7 @@ import { CustomersModule } from './modules/customers/customers.module';
 import { DriversModule } from './modules/drivers/drivers.module';
 import { TripsModule } from './modules/trips/trips.module';
 import * as path from 'path';
+import { LoggerMiddleware } from './utils/logger.middlware';
 
 @Module({
   imports: [
@@ -21,4 +22,8 @@ import * as path from 'path';
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
