148601564befb40e79e8609763f535195928e4c8
oligregz <gregorysak90@gmail.com>
2024-11-26 20:51:46 -0300

    feat: adds coordinates api searcher and sanitized responses

--- api/src/modules/rides/utils/googleMapsConsumer.ts
+++ api/src/modules/rides/utils/googleMapsConsumer.ts
@@ -8,6 +8,7 @@ dotenv.config();
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 const BASE_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json';
+const GEOCODING_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 if (!GOOGLE_API_KEY) {
   throw new AppError(
@@ -17,22 +18,35 @@ if (!GOOGLE_API_KEY) {
   );
 }
 
-export async function getDistanceMatrix(
+export async function getTimeAndDistanceOfRide(
   origin: string,
   destination: string,
   mode: string,
 ): Promise<IDistanceResponse> {
   try {
-    const response = await axios.get(BASE_URL, {
+    // Coordinates getters
+    const originCoordinates = await getCoordinates(origin);
+    const destinationCoordinates = await getCoordinates(destination);
+
+    if (!originCoordinates || !destinationCoordinates) {
+      throw new AppError(
+        ERROR.CODE_DESCRIPTION_STATUS.COORDINATES_NOT_FOUND.CODE,
+        ERROR.CODE_DESCRIPTION_STATUS.COORDINATES_NOT_FOUND.DESCRIPTION,
+        ERROR.CODE_DESCRIPTION_STATUS.COORDINATES_NOT_FOUND.STATUS,
+      );
+    }
+
+    // Use coordinates in Matrix API
+    const distancematrixResponse = await axios.get(BASE_URL, {
       params: {
-        origins: origin,
-        destinations: destination,
+        origins: `${originCoordinates.lat},${originCoordinates.lng}`,
+        destinations: `${destinationCoordinates.lat},${destinationCoordinates.lng}`,
         key: GOOGLE_API_KEY,
         mode,
       },
     });
 
-    const data = response.data;
+    const data = distancematrixResponse.data;
 
     if (data.status !== 'OK') {
       throw new AppError(
@@ -54,13 +68,42 @@ export async function getDistanceMatrix(
       );
     }
 
+    // Prepares the routeResponse object with the sanitized responses
+    const routeResponse = {
+      originCoordinatesGeocodeResponse: sanitizeResponse(originCoordinates),
+      destinationCoordinatesGeocodeResponse: sanitizeResponse(
+        destinationCoordinates,
+      ),
+      distancematrixResponse: sanitizeResponse({
+        status: distancematrixResponse.data.status,
+        origin_addresses: distancematrixResponse.data.origin_addresses,
+        destination_addresses:
+          distancematrixResponse.data.destination_addresses,
+        rows: distancematrixResponse.data.rows,
+        request: {
+          origin: `${originCoordinates.lat},${originCoordinates.lng}`,
+          destination: `${destinationCoordinates.lat},${destinationCoordinates.lng}`,
+          mode,
+        },
+      }),
+    };
+
     return {
       origin: data.origin_addresses[0],
       destination: data.destination_addresses[0],
+      originCoordinates: {
+        lat: originCoordinates.lat,
+        lng: originCoordinates.lng,
+      },
+      destinationCoordinates: {
+        lat: destinationCoordinates.lat,
+        lng: destinationCoordinates.lng,
+      },
       distanceText: element.distance.text,
       distanceValue: element.distance.value,
       durationText: element.duration.text,
       durationValue: element.duration.value,
+      routeResponse,
     };
   } catch (error: any) {
     console.error('Erro ao chamar a API:', error.message);
@@ -70,4 +113,64 @@ export async function getDistanceMatrix(
       ERROR.CODE_DESCRIPTION_STATUS.BAD_DISTANCE_CALCULATION.STATUS,
     );
   }
+}
+
+async function getCoordinates(address: string) {
+  const response = await axios.get(GEOCODING_URL, {
+    params: {
+      address,
+      key: GOOGLE_API_KEY,
+    },
+  });
+
+  const data = response.data;
+
+  if (data.status !== 'OK') {
+    throw new AppError(
+      ERROR.CODE_DESCRIPTION_STATUS.GOOGLE_API_ERROR.CODE,
+      ERROR.CODE_DESCRIPTION_STATUS.GOOGLE_API_ERROR.DESCRIPTION,
+      ERROR.CODE_DESCRIPTION_STATUS.GOOGLE_API_ERROR.STATUS +
+        data.error_message,
+    );
+  }
+
+  const location = data.results[0]?.geometry?.location;
+
+  if (!location) {
+    throw new AppError(
+      ERROR.CODE_DESCRIPTION_STATUS.INVALID_COORDINATES.CODE,
+      ERROR.CODE_DESCRIPTION_STATUS.INVALID_COORDINATES.DESCRIPTION,
+      ERROR.CODE_DESCRIPTION_STATUS.INVALID_COORDINATES.STATUS,
+    );
+  }
+
+  return location;
+}
+
+// Function to clean objects from circular references
+function sanitizeResponse(obj: any): any {
+  const seen = new WeakSet();
+
+  return JSON.parse(
+    JSON.stringify(obj, (key, value) => {
+      // Ignores specific fields that may cause circularity
+      if (
+        key === 'socket' ||
+        key === 'client' ||
+        key === '_httpMessage' ||
+        key === 'httpModule'
+      ) {
+        return undefined;
+      }
+
+      // Treat objects to avoid circular references
+      if (typeof value === 'object' && value !== null) {
+        if (seen.has(value)) {
+          return undefined;
+        }
+        seen.add(value);
+      }
+      return value;
+    }),
+  );
 }
