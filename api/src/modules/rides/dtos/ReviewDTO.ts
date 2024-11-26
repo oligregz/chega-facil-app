8638433387094de01b1b5e3ed185c09528eef3af
oligregz <gregorysak90@gmail.com>
2024-11-26 20:56:33 -0300

    feat: add custom decorator treatment data

--- api/src/modules/rides/dtos/ReviewDTO.ts
+++ api/src/modules/rides/dtos/ReviewDTO.ts
@@ -1,5 +1,9 @@
+import { IsInvalidBodyCustom } from '../classValidator/IsInvalidBodyCustom';
+
 export class ReviewDTO {
+  @IsInvalidBodyCustom()
   rating: number;
 
+  @IsInvalidBodyCustom()
   comment: string;
 }
