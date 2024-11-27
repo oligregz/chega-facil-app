d78f595709e0f1cebfa3b2e22b7b37a0bc0365a9
oligregz <gregorysak90@gmail.com>
2024-11-27 15:41:19 -0300

    fix: add type uuid validation

--- api/src/modules/rides/utils/validateId.ts
+++ api/src/modules/rides/utils/validateId.ts
@@ -3,8 +3,9 @@ import { ERROR } from '../../../errors/errors';
 import { validate } from 'uuid';
 
 export function validateId(id: string, entity: string): boolean {
-  const customerIdIsValid: boolean = !!id && id.length > 0 && id !== undefined;
-  const driverIdIsValid: boolean = id !== undefined || validate(id);
+  const customerIdIsValid: boolean =
+    !!id && id.length > 0 && id !== undefined && validate(id);
+  const driverIdIsValid: boolean = id !== undefined && validate(id);
 
   if (entity === 'customer') {
     if (!customerIdIsValid)
@@ -13,7 +14,8 @@ export function validateId(id: string, entity: string): boolean {
         ERROR.CODE_DESCRIPTION_STATUS.INVALID_DATA.DESCRIPTION,
         ERROR.CODE_DESCRIPTION_STATUS.INVALID_DATA.STATUS,
       );
-
+  }
+  if (entity === 'driver') {
     if (!driverIdIsValid)
       throw new AppError(
         ERROR.CODE_DESCRIPTION_STATUS.INVALID_DATA.CODE,
