49cc55a0923dae6e3d22d6786d699b350153b9dd
oligregz <gregorysak90@gmail.com>
2024-11-27 15:59:21 -0300

    fix: add empty string acceptance

--- api/src/modules/rides/utils/validateId.ts
+++ api/src/modules/rides/utils/validateId.ts
@@ -5,7 +5,9 @@ import { validate } from 'uuid';
 export function validateId(id: string, entity: string): boolean {
   const customerIdIsValid: boolean =
     !!id && id.length > 0 && id !== undefined && validate(id);
-  const driverIdIsValid: boolean = id !== undefined && validate(id);
+  const driverIdIsValid: boolean =
+    (id !== undefined && validate(id)) ||
+    (typeof id === 'string' && id.length === 0);
 
   if (entity === 'customer') {
     if (!customerIdIsValid)
@@ -16,6 +18,7 @@ export function validateId(id: string, entity: string): boolean {
       );
   }
   if (entity === 'driver') {
+    console.log('driver_id', typeof id);
     if (!driverIdIsValid)
       throw new AppError(
         ERROR.CODE_DESCRIPTION_STATUS.INVALID_DATA.CODE,
