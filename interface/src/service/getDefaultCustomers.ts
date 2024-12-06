62da734a52b2de05e798090ed4f750ec33992b7e
oligregz <gregorysak90@gmail.com>
2024-12-06 10:40:19 -0300

    refact: remove useless code

--- interface/src/service/getDefaultCustomers.ts
+++ interface/src/service/getDefaultCustomers.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 export async function getDefaultCustomers() {
   try {
     const response = await axios.get('http://localhost:8080/customers');
-    console.log(response.data)
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
--- interface/src/service/listDriversVailables.ts
+++ interface/src/service/listDriversVailables.ts
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 export async function listDriversDefault(body: unknown) {
   try {
-    console.log(body)
     const response = await axios.post('http://localhost:8080/ride/estimate', body);
-    console.log(response)
     return response.data;
   } catch (error) {
     console.error('Erro desconhecido:', error);
